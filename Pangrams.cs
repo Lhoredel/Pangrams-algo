bfcddaf2599411ea82d6ee06de6e64580b00fe4a
Lhoredel <lhoredeldollentas@gmail.com>
2025-10-03 19:34:00 +0800

    Pangrams.cs

---
+++ Pangrams.cs
@@ -0,0 +1,64 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'pangrams' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string pangrams(string s)
+    {
+        bool[] alphabet = new bool[26];
+        
+        foreach (char c in s)
+        {
+        if (char.IsLetter(c))
+        {
+            int index = char.ToLower(c) - 'a';
+            alphabet[index] = true;
+        }
+    }
+    
+    foreach (bool present in alphabet)
+    {
+        if (!present)
+            return "not pangram";
+    }
+    
+    return "pangram";
+}
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        string result = Result.pangrams(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
